77bdbdcf1de08afe8ef7ab8abdb16cf0fb35e3be
Jun <me@noraesae.net>
2016-09-19 16:23:07 +0900

    Simplify ps_spec

--- test/ps_spec.js
+++ test/ps_spec.js
@@ -4,43 +4,41 @@ import $ from './helpers/query-selector';
 import F from './helpers/fixture';
 
 describe('ps', () => {
-  describe('initialize', () => {
-    beforeEach(() => F.load('ps_spec'));
-    afterEach(() => F.unload());
+  beforeEach(() => F.load('ps_spec'));
+  afterEach(() => F.unload());
 
-    it('parameters', () => {
-      const container = $('#container');
-      const mount = $('#mount');
+  it('parameters', () => {
+    const container = $('#container');
+    const mount = $('#mount');
 
-      expect(() => ps(container, { mount, emulators: [] }))
-        .not.toThrow();
-    });
+    expect(() => ps(container, { mount, emulators: [] }))
+      .not.toThrow();
+  });
 
-    it('selectors', () => {
-      expect(() => ps('#container', { mount: '#mount', emulators: [] }))
-        .not.toThrow();
-    });
+  it('selectors', () => {
+    expect(() => ps('#container', { mount: '#mount', emulators: [] }))
+      .not.toThrow();
+  });
 
-    it('return value', () => {
-      const instance = ps('#container', { mount: '#mount', emulators: [] });
-      expect(instance instanceof Instance).toBe(true);
-    });
+  it('return value', () => {
+    const instance = ps('#container', { mount: '#mount', emulators: [] });
+    expect(instance instanceof Instance).toBe(true);
+  });
 
-    it('wrong parameters', () => {
-      const el = $('#container');
-      const opt = { mount: '#mount', emulators: [] };
+  it('wrong parameters', () => {
+    const el = $('#container');
+    const opt = { mount: '#mount', emulators: [] };
 
-      expect(() => ps(null, opt)).toThrow();
-      expect(() => ps(1234, opt)).toThrow();
-      expect(() => ps('#no-el', opt)).toThrow();
+    expect(() => ps(null, opt)).toThrow();
+    expect(() => ps(1234, opt)).toThrow();
+    expect(() => ps('#no-el', opt)).toThrow();
 
-      expect(() => ps(el, { mount: null, emulators: [] })).toThrow();
-      expect(() => ps(el, { mount: 1234, emulators: [] })).toThrow();
-      expect(() => ps(el, { mount: '#no-el', emulators: [] })).toThrow();
+    expect(() => ps(el, { mount: null, emulators: [] })).toThrow();
+    expect(() => ps(el, { mount: 1234, emulators: [] })).toThrow();
+    expect(() => ps(el, { mount: '#no-el', emulators: [] })).toThrow();
 
-      expect(() => ps(el, { mount: '#mount', emulators: 1234 })).toThrow();
-      expect(() => ps(el, { mount: '#mount', emulators: { } })).toThrow();
-      expect(() => ps(el, { mount: '#mount', emulators: 'foo' })).toThrow();
-    });
+    expect(() => ps(el, { mount: '#mount', emulators: 1234 })).toThrow();
+    expect(() => ps(el, { mount: '#mount', emulators: { } })).toThrow();
+    expect(() => ps(el, { mount: '#mount', emulators: 'foo' })).toThrow();
   });
 });

37882d45328576cf200ccf8b1f4ad271f0a5617e
Jun <me@noraesae.net>
2016-09-22 01:00:44 +0900

    Add initial property initialization

--- src/instance.js
+++ src/instance.js
@@ -1,21 +1,29 @@
 import appendElement from './helpers/append-element';
 
 export default class Instance {
+  static properties() {
+    return {
+      container: null,
+      opts: {},
+      els: {},
+      mutObserver: null,
+      dirty: false,
+    };
+  }
+
   constructor(container, opts) {
+    Object.assign(this, Instance.properties());
+
     this.container = container;
     this.opts = opts;
 
     this.appendScrollbars();
     this.installMutationObserver();
 
-    // initialize properties and flags
-    this.dirty = false;
-
     this.update();
   }
 
   appendScrollbars() {
-    this.els = {};
     this.els.trackX = appendElement(this.opts.mount, 'div', 'ps-track-x');
     this.els.trackY = appendElement(this.opts.mount, 'div', 'ps-track-y');
     this.els.thumbX = appendElement(this.els.trackX, 'div', 'ps-thumb-x');
